c621835a3c5e6aaabddb5564552db0a1234049ba
FEZ <dspeakers8@gmail.com>
2021-08-09 18:50:22 +0300

    add some commands handlers

--- bot.py
+++ bot.py
@@ -7,17 +7,23 @@ bot = telebot.TeleBot(config.token, parse_mode='HTML')
 
 isDebug = config.debug_mode
 
+payment_method_variant = ''
+payment_amount = ''
+
 bot_commands = ['start', 'help', 'print_phone', 'print_token', 'print_account', 'print_service_status']
 
 
 @bot.message_handler(commands=['start'])
-def command_start(message):
+def command_start(message, t = 0):
     set_chat_id(message)
 
     reply_keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True) # , input_field_placeholder='FEZZZ')
-    reply_keyboard.row('Мої рахунки', 'Сплатити')
+    reply_keyboard.add('Сплатити')
+    reply_keyboard.row( 'Мої рахунки', 'Налаштування')
 
-    bot.send_message(config.cid, 'Привіти  <b>' + message.from_user.first_name + '</b>!', reply_markup=reply_keyboard)
+    if t == 0:
+        bot.send_message(config.cid, 'Привіти  <b>' + message.from_user.first_name + '</b>!')
+    bot.send_message(config.cid, 'ГОЛОВНЕ МЕНЮ', reply_markup=reply_keyboard)
 
 
 @bot.message_handler(commands=['help'])
@@ -60,21 +66,100 @@ def command_print_service_status(message):
 def text_message_handler(message):
     set_chat_id(message)
     msg_txt = message.text
-    # bot.reply_to(message, 'Text: ' + message.text)
+
     if msg_txt == 'Сплатити':
-        out_msg_text = '<a href="https://triolan.name">Triolan.NET</a>: ' + str(config.account) + '\n'
-        out_msg_text += 'Послуга '
-        if config.service_status:
-            out_msg_text += 'увімкнена\n'
-        else:
-            out_msg_text += 'вимкнена\n'
+        reply_keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True,
+                                                   input_field_placeholder='Вибери сервіс оплати')
+        reply_keyboard.row('PrivatBank', 'Portmone', 'iPay')
+        reply_keyboard.add('Повернутися')
+
+        out_msg_text = '<b><a href="http://Triolan.net">Triolan.NET</a></b>: ' + str(config.account) + '\n'
+        out_msg_text += get_service_status(config.service_status)
+        out_msg_text += 'Сплачено по: <b>' + config.paid_until + '</b>'
+
+        send = bot.send_message(config.cid, out_msg_text, reply_markup=reply_keyboard)
+        bot.register_next_step_handler(send, chose_payment_service)
+
+    elif msg_txt == 'Повернутися':
+        command_start(message, 1)
+
+    elif msg_txt == 'Мої рахунки':
+        out_msg_text = 'Особові рахунки закріплені за телефоном: <b>' + str(config.account_phone) + '</b>'
         bot.send_message(config.cid, out_msg_text)
+        out_msg_text = '<b><a href="https://Triolan.net">Triolan.NET</a></b>: ' + str(config.account) + '\n'
+        out_msg_text += 'м.Ровно, Вячеслава Черновола улица, буд.51\n'
+        out_msg_text += 'Сплачено по: ' + config.paid_until + '\n'
+        out_msg_text += get_service_status(config.service_status)
+        bot.send_message(config.cid, out_msg_text)
+
+    elif msg_txt == 'Налаштування':
+        out_msg_text = 'Меню Налаштувань'
+        reply_keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        reply_keyboard.row('Статус послуги', 'Кінцева дата')
+        reply_keyboard.add('Повернутися')
+        bot.send_message(config.cid, out_msg_text, reply_markup=reply_keyboard)
+
+    elif msg_txt == 'Статус послуги':
+        reply_keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True,
+                                                   input_field_placeholder='Вибери чи введи статус послуги')
+        reply_keyboard.row('Увімкнути', 'Вимкнути')
+        send = bot.send_message(config.cid, get_service_status(config.service_status), reply_markup=reply_keyboard)
+        bot.register_next_step_handler(send, set_service_status)
+
+    elif msg_txt == 'Кінцева дата':
+        reply_keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True,
+                                                   input_field_placeholder='Дата дії послуги...')
+        reply_keyboard.add('Повернутися')
+        send = bot.send_message(config.cid, 'Сплачено по: ' + config.paid_until, reply_markup=reply_keyboard)
+        bot.register_next_step_handler(send, set_paid_until_date)
+
+
+def set_service_status(message):
+    if message.text == 'Увімкнути':
+        config.set_service_status(True)
+    elif message.text == 'Вимкнути':
+        config.set_service_status(False)
+    bot.send_message(config.cid, get_service_status(config.service_status))
+    command_start(message, 1)
+
+
+def set_paid_until_date(message):
+    if message.text != 'Повернутися':
+        config.set_paid_until_date(message.text)
+    command_start(message, 1)
 
 
 def set_chat_id(message):
     config.cid = message.chat.id
     if isDebug:
         print('Chat ID set to: ' + str(config.cid))
+
+
+def get_service_status(status):
+    msg_text = 'Послуга <b>'
+    if status:
+        msg_text += 'увімкнена</b>\n'
+    else:
+        msg_text += 'вимкнена</b>\n'
+    return msg_text
+
+
+def chose_payment_service(message):
+    global payment_method_variant
+    payment_method_variant = message.text
+    reply_keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True,
+                                               input_field_placeholder='Вибери або введи суму поповнення')
+    reply_keyboard.row('30', '50', '120')
+    send = bot.send_message(config.cid, 'Вибери суму оплати рахунку, або введи самомтійно', reply_markup=reply_keyboard)
+    bot.register_next_step_handler(send, generate_payment_message)
+
+
+def generate_payment_message(message):
+    global payment_amount
+    payment_amount = message.text
+
+    inline_keyboard = types.InlineKeyboardMarkup()
+    inline_keyboard.add()
 
 
 bot.polling()
--- config.py
+++ config.py
@@ -1,13 +1,18 @@
 token = '1775386822:AAElY-QwJK4Z5vR1FtvnLYP_lzhzZIZczo8'
-
-debug_mode = True
-
+debug_mode = False
 cid = 0
-
 main_url = "https://triolan.name"
-
 account = 3658365
-account_phone = '000000000000000'
+account_phone = '380962614481'
 service_status = False
+paid_until = '2021-08-08'
 
-# <a href="http://www.example.com/">inline URL</a>
+
+def set_service_status(service_bool):
+    global service_status
+    service_status = service_bool
+
+
+def set_paid_until_date(date):
+    global paid_until
+    paid_until = date
