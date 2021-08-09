c7d42d1bb5c62c708cdd2f8eb7e5892ab3393f19
FEZ <dspeakers8@gmail.com>
2021-08-09 17:28:36 +0300

    Add payment button

--- bot.py
+++ bot.py
@@ -7,12 +7,17 @@ bot = telebot.TeleBot(config.token, parse_mode='HTML')
 
 isDebug = config.debug_mode
 
-bot_commands = ['start', 'help', 'print_token', 'print_account', 'print_service_status']
+bot_commands = ['start', 'help', 'print_phone', 'print_token', 'print_account', 'print_service_status']
 
 
 @bot.message_handler(commands=['start'])
 def command_start(message):
     set_chat_id(message)
+
+    reply_keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True) # , input_field_placeholder='FEZZZ')
+    reply_keyboard.row('Мої рахунки', 'Сплатити')
+
+    bot.send_message(config.cid, 'Привіти  <b>' + message.from_user.first_name + '</b>!', reply_markup=reply_keyboard)
 
 
 @bot.message_handler(commands=['help'])
@@ -30,6 +35,12 @@ def command_print_token(message):
     bot.send_message(config.cid, 'Token: <code>' + config.token + '</code>')
 
 
+'''@bot.message_handler(commands=['print_phone'])
+def command_print_phone(message):
+    set_chat_id(message)
+    bot.send_message(config.cid, 'Phone: <b>+' + config.account_phone + '</b>')
+
+
 @bot.message_handler(commands=['print_account'])
 def command_print_account(message):
     set_chat_id(message)
@@ -42,17 +53,28 @@ def command_print_service_status(message):
     if config.service_status:
         bot.send_message(config.cid, 'Статус послуги: <i>увімкнена</i>')
     else:
-        bot.send_message(config.cid, 'Статус послуги: <i>вимкнена</i>')
+        bot.send_message(config.cid, 'Статус послуги: <i>вимкнена</i>')'''
 
 
 @bot.message_handler(content_types=["text"])
 def text_message_handler(message):
-    bot.reply_to(message, 'Text: ' + message.text)
+    set_chat_id(message)
+    msg_txt = message.text
+    # bot.reply_to(message, 'Text: ' + message.text)
+    if msg_txt == 'Сплатити':
+        out_msg_text = '<a href="https://triolan.name">Triolan.NET</a>: ' + str(config.account) + '\n'
+        out_msg_text += 'Послуга '
+        if config.service_status:
+            out_msg_text += 'увімкнена\n'
+        else:
+            out_msg_text += 'вимкнена\n'
+        bot.send_message(config.cid, out_msg_text)
 
 
 def set_chat_id(message):
     config.cid = message.chat.id
-    print('Chat ID set to: ' + str(config.cid))
+    if isDebug:
+        print('Chat ID set to: ' + str(config.cid))
 
 
 bot.polling()
--- config.py
+++ config.py
@@ -4,5 +4,10 @@ debug_mode = True
 
 cid = 0
 
+main_url = "https://triolan.name"
+
 account = 3658365
+account_phone = '000000000000000'
 service_status = False
+
+# <a href="http://www.example.com/">inline URL</a>
