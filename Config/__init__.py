09dcb9b870ca42be3edd3697742e26a465dd6488
Denys <dspeakers8@gmail.com>
2022-01-13 02:26:47 +0200

    Update __init__.py
    Removed personal data(token, account contract id)

--- Config/__init__.py
+++ Config/__init__.py
@@ -1,6 +1,4 @@
 # -*- coding: utf-8 -*-
 
-
-Token = '5074195246:AAGClhdTA3IMwjkphYCAj_OupzfrXJE1cyE'  # @triolan_payment_bot
-AccountID = 3658365
-PaidUntil = '2021-08-08'
+Token = '<YOUR_BOT_TOKEN>'
+AccountID = '<YOUR_CONTRACT_ID>'
