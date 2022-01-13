86431a2286847286a39d980c5cd09cd93471691c
Denys <dspeakers8@gmail.com>
2022-01-13 02:13:13 +0200

    Create __init__.py
    __init__ file for Settings module

---
+++ Settings/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# region ──────────╼[ IMPORT ]╾──────────
+from .settings import Settings
+# endregion
+
+# region ──────────╼[ Objects initialization ]╾──────────
+settings_handler = Settings()
+# endregion
